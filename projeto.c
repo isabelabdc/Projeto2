aaa6201e84debf0dd0e308b6f092298742198c13
isabelabdc <ibonoto@yahoo.com>
2025-04-16 16:00:52 -0300

    Funções para salvar e carregar dados de usuarios em arquivos

--- projeto.c
+++ projeto.c
@@ -18,3 +18,28 @@ DataHora data() {
     data.segundo = tm_info->tm_sec;
     return data;
 }
+
+//DADOS DE USUARIOS:
+//funcao para salvar dados de usuarios no arquivo
+void salvarUsuarios(Usuario usuarios[], int total) {
+    FILE *fp = fopen("usuarios.dat", "wb");
+    if (fp) {
+        fwrite(&total, sizeof(int), 1, fp);
+        fwrite(usuarios, sizeof(Usuario), total, fp);
+        fclose(fp);
+    } else {
+        printf("Erro ao salvar arquivo de usuarios!\n");
+    }
+}
+
+//funcao que carrega dados do usuario no arquivo
+int carregarUsuarios(Usuario usuarios[]) {
+    FILE *fp = fopen("usuarios.dat", "rb");
+    int total = 0;
+    if (fp) {
+        fread(&total, sizeof(int), 1, fp);
+        fread(usuarios, sizeof(Usuario), total, fp);
+        fclose(fp);
+    }
+    return total;
+}
